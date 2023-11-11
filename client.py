68121c530eefc09d3023e74ee15f793bde8f52e8
travilist <travish2000@gmail.com>
2023-11-11 14:33:52 -0500

    Untested framework

--- client.py
+++ client.py
@@ -26,11 +26,20 @@ def get_move(player, board):
   board_rows = len(board)
   board_cols = len(board[0])
 
+  # Determine opponent
+  opponent = 2
+
+  if player == 2:
+    opponent = 1
+
   # --- HELPER FUNCTIONS
   # Return the piece from the specified row and column
   # Used to make later code look better
-  def get_stone(pos_row, pos_col):
-    return board[pos_row][pos_col]
+  def get_stone(future_board, pos_row, pos_col):
+    return future_board[pos_row][pos_col]
+  
+  def change_stone(future_board, pos, player_num):
+    future_board[pos[GET_ROW]][pos[GET_COL]] = player_num
 
   # Make sure that location is within board boundaries
   def within_boundaries(pos_row, pos_col):
@@ -40,7 +49,7 @@ def get_move(player, board):
 
   # Search in specified direction for stones that can be flipped
   # Depending on arguments, can search linearly or diagonally
-  def search_direction(position, dir):
+  def search_direction(future_board, player_num, position, dir):
     # Extract search direction from dir array
     dir_row = dir[GET_ROW]
     dir_col = dir[GET_COL]
@@ -49,20 +58,20 @@ def get_move(player, board):
     pos_row = position[GET_ROW] + dir_row
     pos_col = position[GET_COL] + dir_col
 
-    flipped_stones = 0
+    flipped_stones = []
     valid = False
 
     while within_boundaries(pos_row, pos_col):
-      stone = get_stone(pos_row, pos_col)
+      stone = get_stone(future_board, pos_row, pos_col)
 
       if stone == NO_STONE:
         break
       # End loop and mark direction as valid if player's stone is found at the end
-      elif stone == player:
+      elif stone == player_num:
         valid = True
         break
       else:
-        flipped_stones += 1
+        flipped_stones.append([pos_row, pos_col])
 
       # Move in specified direction
       pos_row += dir_row
@@ -74,44 +83,171 @@ def get_move(player, board):
       return 0
 
   # --- DETERMINE VALID MOVES
-  valid_moves = []
+  def find_valid_moves(future_board, player_num):
+    valid_moves = []
 
-  # Determine valid moves
-  for col in range(board_cols):
-    for row in range(board_rows):
-      stone_value = board[row][col]
+    # Determine valid moves
+    for col in range(board_cols):
+      for row in range(board_rows):
+        stone_value = future_board[row][col]
 
-      # Invalid - if a stone is already in the current position
-      if stone_value != 0:
-        continue
+        # Invalid - if a stone is already in the current position
+        if stone_value != 0:
+          continue
 
-      flipped_stones = 0  # Amount of flipped stones found during searching
-      current_pos = [row, col]  # Current loop position, for searching input
+        flipped_stones = []  # Array of positions of flipped stones found during searching
+        current_pos = [row, col]  # Current loop position, for searching input
 
-      search_directions = [
-        [SEARCH_UP, SEARCH_NONE], # UP
-        [SEARCH_DOWN, SEARCH_NONE], # DOWN
-        [SEARCH_NONE, SEARCH_RIGHT], # RIGHT
-        [SEARCH_NONE, SEARCH_LEFT], # LEFT
-        [SEARCH_DOWN, SEARCH_RIGHT], # BOTTOM RIGHT DIAGONAL
-        [SEARCH_UP, SEARCH_RIGHT], # TOP RIGHT DIAGONAL
-        [SEARCH_DOWN, SEARCH_LEFT], # BOTTOM LEFT DIAGONAL
-        [SEARCH_UP, SEARCH_LEFT] # TOP LEFT DIAGONAL
-      ]
+        search_directions = [
+          [SEARCH_UP, SEARCH_NONE], # UP
+          [SEARCH_DOWN, SEARCH_NONE], # DOWN
+          [SEARCH_NONE, SEARCH_RIGHT], # RIGHT
+          [SEARCH_NONE, SEARCH_LEFT], # LEFT
+          [SEARCH_DOWN, SEARCH_RIGHT], # BOTTOM RIGHT DIAGONAL
+          [SEARCH_UP, SEARCH_RIGHT], # TOP RIGHT DIAGONAL
+          [SEARCH_DOWN, SEARCH_LEFT], # BOTTOM LEFT DIAGONAL
+          [SEARCH_UP, SEARCH_LEFT] # TOP LEFT DIAGONAL
+        ]
 
-      # Search all directions for opposing player's stones that can be flipped
-      for direction in search_directions:
-        flipped_stones += search_direction(current_pos, direction)
+        # Search all directions for opposing player's stones that can be flipped
+        for direction in search_directions:
+          flipped_stones += search_direction(future_board, player_num, current_pos, direction)
 
-      if flipped_stones > 0:
-        valid_moves.append([flipped_stones, current_pos])
+        if len(flipped_stones) > 0:
+          valid_moves.append([flipped_stones, current_pos])
+
+    return valid_moves
+  
+  # Valid moves: [[[1, 2], [2, 3], [3, 2]], [1, 3]]
 
   # --- DETERMINE BEST MOVES
-  def get_amount_flipped(valid_move):
-    return valid_move[0]
 
+  def copy_board(input_board):
+    return input_board.copy()
+
+  # Counts the pieces on the board
+  # Returns which player has the majority of stones, as well as if the board is full or not
+  def check_board_conditions(future_board):
+    player_one_stones = 0
+    player_two_stones = 0
+
+    board_full = True
+
+    for col in range(board_cols):
+      for row in range(board_rows):
+        stone = get_stone(future_board, row, col)
+
+        if stone == 0:
+          board_full = False
+        elif stone == 1:
+          player_one_stones += 1
+        elif stone == 2:
+          player_two_stones += 1
+
+    return board_full, player_one_stones, player_two_stones
+
+  # Variables to better help navigate valid_moves array
+  LIST_FLIPPED = 0
+  LIST_POS = 1
+
+  scores = {
+    'Opp has no stones': 5000,
+    'Plr wins': 1000,
+    'Opp has less stones': 10,
+    'Opp has more stones': -50,
+    'Same amount of stones': -10,
+    'Tie game': -1000,
+    'Plr loses': -5000,
+    'Plr has no pieces': -9999
+  }
+
+  def calculate_score(board_full, player_one_stones, player_two_stones):
+    player_stones = player_one_stones
+    opponent_stones = player_two_stones
+
+    if player == 2:
+      player_stones = player_two_stones
+      opponent_stones = player_one_stones
+
+    if opponent_stones == 0:
+      return scores['Opp has no stones']
+    elif player_stones == 0:
+      return scores['Plr has no pieces']
+    
+    majority_stones = 0
+
+    if player_one_stones > player_two_stones:
+      majority_stones = 1
+    elif player_two_stones < player_one_stones:
+      majority_stones = 2
+    
+    if board_full:
+      if majority_stones == player:
+        return scores['Plr wins']
+      elif majority_stones == opponent:
+        return scores['Plr loses']
+      else:
+        return scores['Tie game']
+      
+    if majority_stones == player:
+      return scores['Opp has less stones']
+    elif majority_stones == opponent:
+      return scores['Opp has more stones']
+    else:
+      return scores['Same amount of stones']
+    
+  def make_move(future_board, move, player_num):
+    # Move[[1, 2], [2, 3], [3, 2]], [1, 3]
+
+    change_stone(future_board, move[1], player_num)
+
+    for stone in move[0]:
+      change_stone(future_board, stone, player_num)
+
+
+  def find_best_move(future_board, max_player, iterations_left):
+    player_num = player
+
+    if not max_player:
+      player_num = opponent
+
+    valid_moves = find_valid_moves(future_board, player_num)
+    board_full, player_one_stones, player_two_stones = check_board_conditions(future_board)
+
+    if iterations_left == 0 or board_full or len(valid_moves) == 0:
+      return None, calculate_score(board_full, player_one_stones, player_two_stones)
+    
+    # If end conditions not met, recurse
+    optimal_move = valid_moves[0][1]
+
+    if (max_player):
+      current_score = float('-inf')
+
+      for move in valid_moves:
+        future_board = copy_board(future_board)
+        make_move(future_board, move, player)
+        resulting_score = find_best_move(future_board, False, iterations_left - 1)
+
+        if resulting_score > current_score:
+          current_score = resulting_score
+          optimal_move = move[1]
+    else:
+      current_score = float('inf')
+
+      for move in valid_moves:
+        future_board = copy_board(future_board)
+        make_move(future_board, move, opponent)
+        resulting_score = find_best_move(future_board, True, iterations_left - 1)
+
+      if resulting_score < current_score:
+        current_score = resulting_score
+        optimal_move = move[1]
+
+    return optimal_move, current_score
+    
   # Determine move with the most flipped stones
-  best_move = max(valid_moves, key=get_amount_flipped)[1]
+  amount_iterations = 5
+  best_move = find_best_move(copy_board(board), True, 5)
 
   return best_move
 
